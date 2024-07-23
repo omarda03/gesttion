df06e5b7fbcce10a7c32951d0f7c4b3b919f8c2f
omarda03 <130657252+omarda03@users.noreply.github.com>
2024-07-23 13:50:29 +0100

    update

--- gestion_materiel/gestion_materiel/settings.py
+++ gestion_materiel/gestion_materiel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i#4lrfnlt+vro_#)+akmdbp@z%#hwr_wop12e2!zt*abm_k76p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
@@ -119,10 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
+
 
 
 # Default primary key field type
